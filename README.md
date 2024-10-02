8e6a79532cd7b615ba04b866f07767b8f371b7d9
Jacob Lord <jacoblord02@hotmail.com>
2024-10-01 21:17:39 -0600

    added logic to simulation.py program

--- README.md
+++ README.md
@@ -11,8 +11,7 @@
 
 ## Description of the program
 
-This program is intended to simulate a stock market investment strategy over 5 years. With an initial amount of $1000 in a saving account with 0.5% interest per month and the initial stock price at $100, the investment strategy is to buy stock if its price drops below $95 or sell the stock if its price increases above $110. The program will generate 60 random variables with a uniform distribution U(0,1) in order to simulate this scenario. The stochastic model for the stock price is: next month the price will remain the same has probability 1/2 with probability
-1/4 that it is 5% lower and probability 1/4 that it is 5% higher. 
+This program is intended to simulate a stock market investment strategy over 5 years. With an initial amount of $1000 in a saving account with 0.5% interest per month and the initial stock price at $100, the investment strategy is to buy stock if its price drops below $95 or sell the stock if its price increases above $110. This program will generate 60 random variables with a uniform distribution U(0,1) in order to simulate this scenario for 60 months (5 years). The stochastic model for the stock price is: next month the price will remain the same has probability 1/2, probability 1/4 that it is 5% lower, and probability 1/4 that it is 5% higher. 
 
 It is assumed that there are no transaction costs to buy or sell stock.
 
--- simulation.py
+++ simulation.py
@@ -1,2 +1,56 @@
 import random
 
+def init_rand_nums(num_list, n):
+    '''
+    Generates n amount of random float numbers and stores them in a list
+    Parameters: num_list - empty list to store nums
+                n - amount of numbers to generate
+    Returns: list with n amount of random float values
+    '''
+    for i in range(n):
+        rand_val = random.random()
+        num_list.append(rand_val)
+
+random_num_list = [] #declare list to hold randomly generated numbers
+bank_acct_amt = 1000 #initial investment amount
+months = 6 #number of months to simulate; 5 years in this case
+stock_price = 100 #initial stock price
+interest_amt = 0 #amount of interest made from savings 
+interest_rate = 0.005 #interest rate of savings account if money is held in there
+change_rate = 0.05 #rate of change for stock every month if it changes
+buy_limit = 95 #boundary point where we choose to convert our money to stock
+sell_limit = 110 #boundary point where we choose to sell our stock for money
+stock_amt = 0 #amount of stock held by user
+
+init_rand_nums(random_num_list, months) #initialize random number list with 60 randomly generated numbers to represent 60 months (5 years)
+
+for i in range(months):
+    rand_val = random_num_list[i]
+
+    if (rand_val < 0.5): #hold case
+        #do nothing?
+        #print('Did nothing for month ' + str(i))
+        print()
+    elif (rand_val >= 0.5 and rand_val < 0.75): #stock decrease case
+        #stock decreased in value
+        price_change = stock_price * change_rate
+        stock_price -= price_change
+    elif (rand_val >= 0.75 and rand_val < 1): #stock increase case
+        #stock increased in value
+        price_change = stock_price * change_rate
+        stock_price += price_change
+    else:
+        print("Error un-uniform distribution??")
+        exit(1)
+
+    #condition to buy stock
+    if (stock_price < 95 and bank_acct_amt > 0):
+            stock_amt = bank_acct_amt / stock_price #calculate amount of stock that can be purchased and "purchase" it
+            bank_acct_amt = 0 #set amount to 0 because we bought stock with it
+    elif (stock_price > 110 and stock_amt > 0): # condition to sell stock for money
+            bank_acct_amt = stock_amt * stock_price #calculate how much the stock is worth and "sell" it
+            stock_amt = 0 #set stock amount to 0 because we sold it
+
+    print("bank amt: " + str(bank_acct_amt) + " stock amt: " + str(stock_amt) + " stock price: " + str(stock_price))
+
+
