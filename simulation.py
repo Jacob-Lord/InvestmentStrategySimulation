cb576c9f48076c5fa4260d42cf734cb747ec2940
Jacob Lord <jacoblord02@hotmail.com>
2024-10-05 13:46:29 -0600

    Formatted Simulation output

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 import random
 import time
+from tabulate import tabulate
 
 def init_rand_nums(num_list, n):
     '''
@@ -54,6 +55,7 @@ def sell_stock(bank_acct_amt, stock_amt, stock_price):
 
 def main():
     random_num_list = [] #declare list to hold randomly generated numbers
+    results_table = [] #table to store results of simulation every month
     bank_acct_amt = 1000 #initial investment amount
     months = 60 #number of months to simulate; 5 years in this case
     stock_price = 100 #initial stock price
@@ -63,6 +65,8 @@ def main():
     buy_limit = 95 #boundary point where we choose to convert our money to stock
     sell_limit = 110 #boundary point where we choose to sell our stock for money
     stock_amt = 0 #amount of stock held by user
+    table_headers = ["Month", "Random Value", "New Stock Price", "Action", "Number of Stocks", "Worth", "Bank Balance", "Interest Amount", "New Bank Balance"] #explicit headers for the table output of simulation results
+    action = "Hold" #action taken by user
 
     init_rand_nums(random_num_list, months) #initialize random number list with 60 randomly generated numbers to represent 60 months (5 years)
 
@@ -74,15 +78,28 @@ def main():
         #condition to buy stock
         if (stock_price < buy_limit and bank_acct_amt > 0):
             bank_acct_amt, stock_amt = buy_stock(bank_acct_amt, stock_amt, stock_price) #call buy stock function
+            action = "Buy"
         #condition to sell stock for money
         elif (stock_price > sell_limit and stock_amt > 0):
             bank_acct_amt, stock_amt = sell_stock(bank_acct_amt, stock_amt, stock_price) #call sell stock function
+            action = "Sell"
+        #default condition where assets are held
+        else:
+            action = "Hold"
+        
+        #calculate interest and bank account information
+        interest_amt = bank_acct_amt * interest_rate #calculate amount gained from interest
+        old_bank_acct_amt = bank_acct_amt #track amount before savings interest is applied
+        bank_acct_amt += interest_amt #add interest amount to savings account
 
-        #if there is money in the savings account, apply interest rate to it and add the amount to the account.
-        if (bank_acct_amt > 0): 
-            interest_amt = bank_acct_amt * interest_rate #calculate amount gained from interest
-            bank_acct_amt += interest_amt #add interest amount to savings account
+        #calculate worth of stock every iteration
+        stock_worth = stock_amt * stock_price 
+        
+        #track simulation results in 2D table
+        results_table.append([i+1, rand_val, stock_price, action, stock_amt, stock_worth, old_bank_acct_amt, interest_amt, bank_acct_amt])
 
-        print("bank amt: " + str(bank_acct_amt) + " stock amt: " + str(stock_amt) + " stock price: " + str(stock_price))
+    #tabulate() converts the results table  list into a table format and puts the defined header values at the top
+    results_tabulated = tabulate(results_table, headers = table_headers)
+    print(results_tabulated)
 
-main()
+main() #run simulation program
